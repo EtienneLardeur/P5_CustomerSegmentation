2e0953395e88453c39f296ad0613167e44e46314
Etienne Lardeur <etienne.lardeur@gmail.com>
2020-09-09 18:30:18 +0200

    EDA : the end?
    with slides updated

--- functions.py
+++ functions.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def distribution(df, feature, xlbl, style):
@@ -9,10 +13,6 @@ def distribution(df, feature, xlbl, style):
     # args:   style = box, violin, dist
     # output : graphe
 
-    import pandas as pd
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import seaborn as sns
 
     plt.figure(figsize=(11, 4))
     x = df.loc[~df[feature].isnull(),
@@ -29,4 +29,57 @@ def distribution(df, feature, xlbl, style):
     plt.yticks([])
     plt.show()
     
+    
+def charm_price(df, price_feature):
+    # fonction qui isole la terminaison "9" des prix affiché, dite "charm price"
+    # input : df = DataFrame
+    #         price_feature = feature
+    # output : le df avec une nouvelle colonne "charm_price"
+    # isoler les terminaisons (Centavos, Real)
 
+    df['last_centavos'] = df[price_feature] % 1
+    df['last_real'] = df[price_feature] % 10 - df[price_feature] % 1
+    df['last_ten_real'] = ((df[df[price_feature] % 10 == 0][price_feature]) / 10) % 10 
+    # convertir en info booléen "is_charm" pour unités et décimales
+    df['charm_centavos'] =\
+        df['last_centavos'].map(
+        lambda x: 0 if x < 0.88 else 1)
+    df['charm_real'] =\
+        df['last_real'].map(
+        lambda x: 0 if x != 9 else 1)
+    df['charm_ten_real'] =\
+        df['last_ten_real'].map(
+        lambda x: 0 if x != 9 else 1)
+    # déterminer un booléen "charm_price" si l'une des terminaisons est "charm"
+    df['charm_price'] = df['charm_real'] + df['charm_centavos'] + df['charm_ten_real']
+    df['charm_price'] =\
+        df['charm_price'].map(
+        lambda x: 1 if x >= 1 else 0)
+    # retirer les colonnes obsolètes
+    Drop = ['last_centavos',
+            'last_real',
+            'last_ten_real',
+            'charm_centavos',
+            'charm_real',
+            'charm_ten_real']
+    df.drop(Drop, axis=1, inplace=True)
+    return(df)
+
+
+def haversine(lat1, lon1, lat2, lon2, to_radians=True, earth_radius=6371):
+    """
+    seen on stackoverflow.com
+
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees or in radians)
+
+    All (lat, lon) coordinates must have numeric dtypes and be of equal length.
+
+    """
+    if to_radians:
+        lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
+
+    a = np.sin((lat2-lat1)/2.0)**2 + \
+        np.cos(lat1) * np.cos(lat2) * np.sin((lon2-lon1)/2.0)**2
+
+    return earth_radius * 2 * np.arcsin(np.sqrt(a))
